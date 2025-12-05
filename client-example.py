97ccdf892afcae514b6b1ca68850027345cad755
DEVELOPERcreation <168726830+DEVELOPERcreatinon@users.noreply.github.com>
2025-12-05 10:39:05 +0900

    Update client-example.py

--- client-example.py
+++ client-example.py
@@ -1,3 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+# 
+# Copyright (c) 2025 developercreation
+
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
 from datetime import datetime
@@ -1835,3 +1841,4 @@ if __name__ == "__main__":
     calculator = VimStyleCalculator()
 
     calculator.run()
+
